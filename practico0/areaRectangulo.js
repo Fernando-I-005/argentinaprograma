65fcce2e7fb6d377c3dc0c92a3c1ca3c6ea77522
Fernando-I-005 <fernandoiglesias05@gmail.com>
2023-05-20 16:39:21 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,11 @@
+let base          // ejercicio 2a
+let altura
+let area=base*altura
+base=5
+altura=3
+area=base*altura
+console.log(area)
+base=12.5         // ejercicio 2b
+altura=5.3
+area=base*altura
+console.log(area)
---
+++ practico0/promedio3.js
@@ -0,0 +1,24 @@
+let num1
+let num2
+let num3
+let promedio=((num1+num2+num3)/3)
+
+num1=5                       //ejercicio 4a
+num2=7
+num3=9
+promedio=((num1+num2+num3)/3)
+console.log(promedio)
+
+num1=-2                      //ejercicio 4a
+num2=7
+num2=-3
+num3=-8
+promedio=((num1+num2+num3)/3)
+console.log(promedio)
+
+num1=-1.2                    //ejercicio 4a
+num2=7
+num2=5.3
+num3=-3.3
+promedio=((num1+num2+num3)/3)
+console.log(promedio)
