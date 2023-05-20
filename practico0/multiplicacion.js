cb798065cbe49e89527f68f2838361cae6cac306
Fernando-I-005 <fernandoiglesias05@gmail.com>
2023-05-20 15:56:06 -0300

    commit inicial

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,5 @@
+let num1=2              // ejercicio 1b
+let num2=17
+let num3=6
+let multiplicacion=num1*num2*num3
+console.log(multiplicacion)
---
+++ practico0/resta.js
@@ -0,0 +1,6 @@
+let minuendo;   // ejercicio 1a
+let sustraendo;
+minuendo=10;
+sustraendo=2;
+let resta= minuendo-sustraendo;
+console.log(resta);
