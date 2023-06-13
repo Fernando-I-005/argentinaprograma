c702635236ad925a7d634d7c8e4aecd06281823b
Fernando-I-005 <fernandoiglesias05@gmail.com>
2023-06-13 19:47:43 -0300

    subida de practico1 en rama main

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,14 @@
+//1a. Defina una variable nombre.
+let nombre;
+
+//1b. Guarde en la variable nombre tu nombre real.
+nombre="Fernando";  
+
+//1c. Defina y asigne, en un mismo paso, la variable apellido. El valor a asignar deberá ser tu apellido real.
+let apellido="Iglesias"; 
+
+//1d. Repita el paso anterior, pero esta vez definiendo la variable edad y asignandole tu edad real.
+let edad= 44; 
+
+//1e. Por último, imprime por consola un mensaje que respete el siguiente formato: "Hola, [nombre] [apellido]. Tienes [edad] años."
+console.log("Hola, %s %s.Tienes %i años.", nombre,apellido,edad);  
---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,6 @@
+//2a. Defina una variable de tipo objeto que lleve el nombre persona, y que contenga las variables nombre, apellido y edad del ejercicio anterior con sus respectivos valores.
+let persona={nombre:"Fernando",apellido:"Iglesias",edad:34};
+
+//2b. Imprime por consola un mensaje que respete el siguiente formato: “Mi objeto persona es el siguiente: [persona]”
+//Observación: Deberás usar la función JSON.stringify para poder imprimir por consola el objeto persona de una manera legible.
+console.log("Mi objeto persona es el siguiente:"+ JSON.stringify (persona));  
